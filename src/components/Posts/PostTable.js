ee5b43ca4537d3387fcef1900f60fd47174d55ae
Shubhangi <shubhangi@kobo360.com>
2021-05-14 14:01:58 +0530

    Fetch data from API

--- src/components/Posts/PostTable.js
+++ src/components/Posts/PostTable.js
@@ -5,12 +5,27 @@ import Data from "../../data.json";
 class PostTable extends Component {
     constructor(props) {
         super(props);
-        this.state = {dataSource: []};
+        this.state = {
+            dataSource: [],
+            pageNumber: 0,
+        };
     }
 
     componentDidMount() {
+
         console.log("data: ", Data.hits);
-        this.setState({dataSource: Data.hits})
+        this.fetchData();
+    }
+
+    fetchData = () => {
+        fetch(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${this.state.pageNumber}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({dataSource: result.hits})
+                },
+                (error) => {console.log("error: ", error);}
+            )
     }
 
     render() {
